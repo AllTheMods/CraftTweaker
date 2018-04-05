2f9739506aecd39509e369703d0d7261449c0cac
BloodWorkXGaming <BloodWorkXGaming@users.noreply.github.com>
2018-04-05 18:47:34 +0200

    test to fix recipe book

--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/asmfun/CrTRecipeListTransformerHelper.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/asmfun/CrTRecipeListTransformerHelper.java
@@ -2,7 +2,10 @@ package crafttweaker.mc1120.asmfun;
 
 
 import org.objectweb.asm.*;
-import org.objectweb.asm.tree.*;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.io.*;
+
 /* http://www.programering.com/a/MzMwgzMwATM.html */
 public class CrTRecipeListTransformerHelper {
     
@@ -10,26 +13,58 @@ public class CrTRecipeListTransformerHelper {
         final ClassNode clazz = ASMUtils.createClassFromByteArray(classBytes);
     
         System.out.println("Transforming clazz: " + name);
-    
+        /*ClassReader cr = new ClassReader(classBytes);
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+        ClassVisitor cv = new ClassVisitor(Opcodes.ASM5, cw){};
+        cr.accept(cv, Opcodes.ASM5);*/
+        
         MethodVisitor mw = clazz.visitMethod(Opcodes.ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
     
-    
+        /*
         // pushes the 'out' field (of type PrintStream) of the System class
         mw.visitFieldInsn(Opcodes.GETSTATIC,"java/lang/System","out","Ljava/io/PrintStream;");
         // pushes the "Hello World!" String constant
         mw.visitLdcInsn("this is equals method print!");
         // invokes the 'println' method (defined in the PrintStream class)
-        mw.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream","println","(Ljava/lang/String;)V", false);
+        mw.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream","println","(Ljava/lang/String;)V", false);*/
         mw.visitInsn(Opcodes.ICONST_1);
         mw.visitInsn(Opcodes.RETURN);
-        mw.visitMaxs(0, 0);
+        mw.visitMaxs(1, 2);
+        mw.visitEnd();
+
+
+        MethodVisitor mw2 = clazz.visitMethod(Opcodes.ACC_PUBLIC, "test", "(Ljava/lang/Object;)V", null, null);
+    
+        /*
+        // pushes the 'out' field (of type PrintStream) of the System class
+        mw.visitFieldInsn(Opcodes.GETSTATIC,"java/lang/System","out","Ljava/io/PrintStream;");
+        // pushes the "Hello World!" String constant
+        mw.visitLdcInsn("this is equals method print!");
+        // invokes the 'println' method (defined in the PrintStream class)
+        mw.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream","println","(Ljava/lang/String;)V", false);*/
+        mw.visitInsn(Opcodes.RETURN);
+        mw.visitMaxs(1, 1);
         mw.visitEnd();
     
-        for(MethodNode method : clazz.methods) {
+        /*for(MethodNode method : cw.methods) {
             System.out.println("method = " + method.name);
+        }*/
+    
+    
+        byte[] code = ASMUtils.createByteArrayFromClass(clazz, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        OutputStream fos;
+        try {
+            fos = new FileOutputStream("D:\\Users\\jonas\\Documents\\GitHub\\CraftTweaker\\CraftTweaker2-MC1120-Main\\src\\main\\java\\crafttweaker\\mc1120\\asmfun\\ExportedClass.class");
+            fos.write(code);
+            fos.close();
+    
+        } catch(IOException e) {
+            e.printStackTrace();
         }
+    
+        return code;
         
-        return ASMUtils.createByteArrayFromClass(clazz, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        // return
     }
     
     @Override
--- CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/events/ClientEventHandler.java
+++ CraftTweaker2-MC1120-Main/src/main/java/crafttweaker/mc1120/events/ClientEventHandler.java
@@ -7,12 +7,18 @@ import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.tooltip.IngredientTooltips;
 import crafttweaker.mc1120.formatting.IMCFormattedString;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.recipebook.RecipeList;
 import net.minecraft.client.settings.*;
 import net.minecraft.client.util.RecipeBookClient;
+import net.minecraft.stats.RecipeBook;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.*;
 import net.minecraftforge.fml.relauncher.*;
+import org.objectweb.asm.*;
+
+import java.io.*;
+import java.util.Arrays;
 
 public class ClientEventHandler {
     private static boolean alreadyChangedThePlayer = false;
@@ -44,5 +50,30 @@ public class ClientEventHandler {
             RecipeBookClient.rebuildTable();
             CraftTweakerAPI.logInfo("Fixed the RecipeBook");
         }
+    
+        RecipeList test = new RecipeList();
+        RecipeList test2 = new RecipeList();
+    
+    
+        try {
+            ClassReader cr = new ClassReader("net.minecraft.client.gui.recipebook.RecipeList");
+            ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+            
+            byte[] code = cw.toByteArray();
+            
+            
+            OutputStream fos = new FileOutputStream("D:\\Users\\jonas\\Documents\\GitHub\\CraftTweaker\\CraftTweaker2-MC1120-Main\\src\\main\\java\\crafttweaker\\mc1120\\asmfun\\ExportedClassLater.class");
+            fos.write(code);
+            fos.close();
+            
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("test.getClass().getDeclaredMethods() = " + Arrays.toString(test.getClass().getDeclaredMethods()));
+    
+    
+        System.out.println("test2.equals(test) = " + test2.equals(test));
+        
+        
     }
 }
